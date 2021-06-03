dd6e03df616a3f059cb6c8e1b276f472115eb08d
dragsec <draganyes@gmail.com>
2021-06-03 15:50:59 +0200

    finale

--- domande.php
+++ domande.php
@@ -27,8 +27,8 @@ return [
     [
         "question" => "Perché il mio account è associato a un paese?",
         "answer" => "
-        <ol>Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose: 
-        <br>
+        Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose: 
+        <ol>
             <li>La società consociata Google che offre i servizi, tratta le tue informazioni ed è responsabile del rispetto delle leggi sulla privacy vigenti. Generalmente Google offre i propri servizi per i consumatori tramite una delle due società seguenti:
                 <ol style='list-style-type: lower-alpha;'>
                     <li>Google Ireland Limited, se gli utenti sono residenti nello Spazio economico europeo (paesi dell'Unione europea, oltre a Islanda, Liechtenstein e Norvegia) o in Svizzera.</li>
